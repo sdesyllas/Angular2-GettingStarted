05f6ae339620057431f2b04482c7a880f4551b2a
Spiros Desyllas <sdesyllas@hotmail.com>
2016-10-13 16:18:39 +0100

    more on components

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -3,13 +3,18 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+
 import { ProductListComponent } from './products/product-list.component'
+
+import { ProductFilterPipe } from './products/product-filter.pipe';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule],
   declarations: 
     [ AppComponent, 
-    ProductListComponent ],
+      ProductListComponent ,
+      ProductFilterPipe
+    ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { IProduct } from './product'
 
 @Component({
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls: ['app/products/product-list.component.css']
 })
 
 
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
@@ -37,5 +39,9 @@ export class ProductListComponent{
 
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void{
+        console.log('ProductListComponent is in OnInit');
     }
 }
