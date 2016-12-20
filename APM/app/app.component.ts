9a0fd0abdf6eb84b9c3f9e7ddd472f8cd1907150
Spiros Desyllas <sdesyllas@hotmail.com>
2016-12-20 15:02:41 +0000

    Navigation and Routing Additional Techniques

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core'
-import { ProductListComponent } from './products/product-list.component';
 import { ProductService } from './products/product-service';
 
 @Component({
--- APM/app/products/product-detail.component.ts
+++ APM/app/products/product-detail.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IProduct } from './product';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl : 'app/products/product-detail.component.html'
 })
 
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+
+    constructor(private _route: ActivatedRoute, private _router: Router){
+
+    }
+
+    ngOnInit(): void{
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+    }
+
+    onBack(): void{
+        this._router.navigate(['/products'];)
+        
+    }
 
     pageTitle: string = "Product details";
     product: IProduct;
